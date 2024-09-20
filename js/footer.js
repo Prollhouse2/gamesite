9db819274d0b8c22418ca40f5cf8e52a9cbda669
Cyber Panel <prollhouse4@gmail.com>
2024-09-20 10:12:21 -0400

    BTD5 GAME CHEATS

--- js/footer.js
+++
@@ -1,7 +0,0 @@
-  <a href="https://discord.com" class="icon" target="_blank" title="Discord">
-        <i class="fab fa-discord"></i>
-    </a>
-    
-    <a href="https://github.com" class="icon" target="_blank" title="GitHub">
-        <i class="fab fa-github"></i>
-    </a>
