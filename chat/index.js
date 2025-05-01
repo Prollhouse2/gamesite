f016f1e6e784d8fc3f548d9b30cce527bbd0f650
Taz ♥ <isavyxd@outlook.com>
2025-05-01 16:04:40 -0400

    Update index.js

--- chat/index.js
+++ chat/index.js
@@ -18,4 +18,45 @@ document.addEventListener("DOMContentLoaded", () => {
       icon.classList.add("fa-expand")
     }
   })
-})
+ particlesJS('snow', {
+            particles: {
+                number: {
+                    value: 150,
+                    density: { enable: true, value_area: 800 }
+                },
+                color: { value: "#ffffff" },
+                shape: {
+                    type: "circle",
+                    stroke: { width: 0, color: "#000000" }
+                },
+                opacity: {
+                    value: 0.5,
+                    random: false,
+                    anim: { enable: false }
+                },
+                size: {
+                    value: 3,
+                    random: true,
+                    anim: { enable: false }
+                },
+                line_linked: { enable: false },
+                move: {
+                    enable: true,
+                    speed: 2,
+                    direction: "bottom",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false
+                }
+            },
+            interactivity: {
+                detect_on: "canvas",
+                events: {
+                    onhover: { enable: true },
+                    onclick: { enable: false },
+                    resize: true
+                }
+            },
+            retina_detect: true
+        });
