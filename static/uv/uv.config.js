2840928c8a8c983b823e41e45c49c4ce50496c8e
Taz ♥ <isavyxd@outlook.com>
2025-05-04 16:10:05 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,11 +1,10 @@
 self.__uv$config = {
-	prefix: "/static/proll/",
-	bare: "https://science.petezahgames.com/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/static/uv/uv.handler.js",
-	bundle: "/static/uv/uv.bundle.js",
-	config: "/static/uv/uv.config.js",
-	sw: "/static/uv/uv.sw.js",
+    prefix: "/static/proll/",
+    bare: "https://v2202412246404304352.megasrv.de/bare/",
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/static/uv/uv.handler.js",
+    bundle: "/static/uv/uv.bundle.js",
+    config: "/static/uv/uv.config.js",
+    sw: "/static/uv/uv.sw.js",
 };
-
