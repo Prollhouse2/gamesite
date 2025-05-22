4f89b7d95a630324d48ded1596ae32122216ff34
Cyber Panel <prollhouse4@gmail.com>
2025-05-22 10:34:48 -0500

    bareserverchange

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/static/proll/",
-    bare: "https://bare.benrogo.net",
+    bare: "https://r2fh2387hv2b2rfbiz.site",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/static/uv/uv.handler.js",
